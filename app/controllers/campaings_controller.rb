68bcc5975a3508cf509531502790e0dade7a89c2
epreron <rodrigometal2010@gmail.com>
2019-04-29 15:54:04 +0100

    Add controllers

---
+++ app/controllers/campaings_controller.rb
@@ -0,0 +1,19 @@
+class CampaingsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def raffle
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+  get 'members/destroy'
+  get 'members/update'
+  get 'campaings/show'
+  get 'campaings/index'
+  get 'campaings/create'
+  get 'campaings/update'
+  get 'campaings/destroy'
+  get 'campaings/raffle'
+  get 'pages/home'
 devise_for :users, :controllers => { registrations: 'registrations' }
 mount Sidekiq::Web => '/sidekiq'
 end
